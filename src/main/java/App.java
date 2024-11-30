5e11a816469c05635bf9bb5385d0594a3af824ca
BomJak <denis250597@yandex.ru>
2024-12-01 01:43:41 +0300

    make bean cat

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,23 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+
+        HelloWorld bean = applicationContext.getBean(HelloWorld.class);
         System.out.println(bean.getMessage());
+
+        HelloWorld secondBean = applicationContext.getBean(HelloWorld.class);
+        System.out.println("Equals Hello World: " + bean.equals(secondBean));
+
+        Cat cat = applicationContext.getBean(Cat.class);
+        Cat secondCat = applicationContext.getBean(Cat.class);
+
+        System.out.println("First cat: " + cat.getName() +
+                            " Second cat: " + secondCat.getName());
+        System.out.println("Equals Cat: " + cat.equals(secondCat));
     }
 }
